6c9ed5882c4ada02922be25fd497e655e1b45aa8
prashantpaddune <padadunep@gmail.com>
2023-11-09 12:36:09 +0530

    chore: bar chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import useGetData from "./hooks/useGetData";
+import { BarChart } from "./components/BarChart";
 
 const App: React.FC = () => {
     const { status, data, error } = useGetData();
 
-    if (status === 'loading') return <div>Loading...</div>;
-    if (status === 'failed') return <div>Error: {error}</div>;
+    if (status === 'loading') {
+        return <div>Loading...</div>;
+    }
+
+    if (status === 'failed' || error) {
+        return <div>Error: {error}</div>;
+    }
+
+    const {
+        category_distribution = {},
+    } = data?.[0] || {};
 
     return (
         <div>
-            {JSON.stringify(data)}
+            <h1>Queries Per Category</h1>
+            <BarChart data={category_distribution} title="Category Distribution" />
         </div>
     );
 };
---
+++ src/components/BarChart/index.tsx
@@ -0,0 +1,65 @@
+// BarChartComponent.tsx
+
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+interface BarChartProps {
+    data: { [category: string]: number };
+    title?: string;
+}
+
+export const BarChart: React.FC<BarChartProps> = ({ data, title }) => {
+    // Transform the data into a format Chart.js can understand
+    const chartData = {
+        labels: Object.keys(data),
+        datasets: [
+            {
+                label: 'Queries',
+                data: Object.values(data),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: !!title,
+                text: title,
+            },
+        },
+    };
+
+    return <Bar data={chartData} options={options} />;
+};
